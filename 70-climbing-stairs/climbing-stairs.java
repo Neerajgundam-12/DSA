23b105532bb3619e0b3051900d87105976c7ff05
Neeraj Gundam <neerajgundam12@gmail.com>
2026-07-05 10:34:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (17.39%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        int ans = 0;
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+    ans = helper(n,dp);
+    return ans;
+    }
+    int helper(int n,int[] dp) {
+        if(n<0)
+        return 0;
+        if(n==0 || n==1)
+        return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+        dp[n]= helper(n-1,dp)+helper(n-2,dp);
+        return dp[n];
+    }
+}
