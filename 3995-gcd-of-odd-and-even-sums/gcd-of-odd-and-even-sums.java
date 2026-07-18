5c5c8f39f4644afbbfeb0785620fdd2a31dd5785
Neeraj Gundam <neerajgundam12@gmail.com>
2026-07-18 15:34:08 +0530

    Time: 8 ms (7.08%) | Memory: 42.8 MB (8.45%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,33 +1,26 @@
 class Solution {
-   // public int gcdOfOddEvenSums(int n) {
-      /*  int sm_odd=1,sm_even=2,cn=1,even_val=2,odd_val=2;
+    public int gcdOfOddEvenSums(int n) {
+        int odd_sm=1,even_sm=2,cn=1,even_val=2,odd_val=1;
         while(cn!=n) {
-            sm_even+=even_val;
-           even_val+=2;
+            odd_val+=2;
+            odd_sm+=odd_val;
             cn++;
         }
         cn=1;
         while(cn!=n) {
-            sm_odd+=odd_val;
-            odd_val+=2;
+            even_val+=2;
+            even_sm+=even_val;
             cn++;
         }
-        System.out.println(sm_odd);
-        System.out.println(sm_even);
-        return Gcd(sm_odd,sm_even);
+        System.out.println(odd_sm);
+        System.out.println(even_sm);
+        return gcd(odd_sm,even_sm);
     }
-    int Gcd(int a,int b) {
-       int r = b%a;
-       if(r==0)
-       return a;
-       return Gcd(r,a);
-
-    }*/
-     private int gcd(int x, int y) {
-        return y == 0 ? x : gcd(y, x % y);
-    }
-
-    public int gcdOfOddEvenSums(int n) {
-        return gcd(n * n, n * (n + 1));
+    int gcd(int a,int b) {
+        int r = b%a;
+        if(r==0)
+        return a;
+        return gcd(r,a);
+        
     }
 }
