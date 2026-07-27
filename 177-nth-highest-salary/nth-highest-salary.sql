1d4976531b325fc3eccb8655e9fc904af2edf2f0
Neeraj Gundam <neerajgundam12@gmail.com>
2026-07-27 23:29:05 +0530

    Time: 501 ms (27.81%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -2,14 +2,7 @@ CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
   RETURN (
       # Write your MySQL query statement below.
-        SELECT salary
-      FROM Employee e1
-      WHERE N - 1 =
-      (
-          SELECT COUNT(DISTINCT e2.salary)
-          FROM Employee e2
-          WHERE e1.salary < e2.salary
-      )
-      LIMIT 1
+        select salary from (select salary,DENSE_RANK() over (order by salary desc) as dr
+         from Employee) as t where dr=N limit 1
   );
 END
